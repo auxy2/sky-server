1c6f86323ab42e722b83f043296a170348bc7292
ABDULLAHI <Ashleyalvins010@gmail.com>
2023-10-14 21:50:34 +0100

    Update transactionrouter

--- routes/webHooks.js
+++ routes/webHooks.js
@@ -1,11 +1,10 @@
-const express = require('express');
-const notification = require('../notifications/notify.ng.paystack')
-const alart = require('../notifications/rateAlart')
+const express = require("express");
+const notification = require("../notifications/notify.ng.paystack");
+const alart = require("../notifications/rateAlart");
 
-const router = express.Router()
+const router = express.Router();
 
+router.post("/webHook-payStack", notification.notify);
+// router.post('/alarts', alart.rateAlart);
 
-router.post('/webHook-payStack', notification.notify)
-router.post('/alarts', alart.rateAlart);
-
-module.exports = router
+module.exports = router;
