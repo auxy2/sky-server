30d38d10006bedb1a9825ec53be9ad78b6f9a39a
ABDULLAHI <Ashleyalvins010@gmail.com>
2023-10-14 20:52:17 +0100

    added some files

---
+++ routes/webHooks.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const notification = require('../notifications/notify.ng.paystack')
+const alart = require('../notifications/rateAlart')
+
+const router = express.Router()
+
+
+router.post('/webHook-payStack', notification.notify)
+router.post('/alarts', alart.rateAlart);
+
+module.exports = router
