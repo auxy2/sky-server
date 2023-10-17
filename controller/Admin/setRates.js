487b3e02564d090693aaf12cc695d8253a1df20b
ABDULLAHI <Ashleyalvins010@gmail.com>
2023-10-17 08:30:27 +0100

    Updated

--- controller/Admin/setRates.js
+++ controller/Admin/setRates.js
@@ -75,23 +75,30 @@ exports.setRate = catchAsync(async (req, res, next) => {
 exports.setGiftCardRate = catchAsync(async (req, res, next) => {
   const rates = await Rates.findOne({ Admin: "Admin" });
 
-  const catBodyObj = req.body.Catigory;
-  const subCatBodyObj = req.body.subCatigory;
+  let Cat_SubBodyObj = req.body;
 
-  let id = Math.random() * date.now();
+  let id = Math.random() * Date.now();
   id = Math.floor(id);
-  catBodyObj ? (catBodyObj.id = id) : (subCatBodyObj.id = id);
-  console.log(id);
-  const catNewRate = [...rates.gitCard_Cartigories, catBodyObj];
-  const SubCatNewRate = [...rates.giftCardSub_Cartigories, subCatBodyObj];
+  Cat_SubBodyObj.id = id;
 
-  rates.gitCard_Cartigories = catNewRate;
-  rates.giftCardSub_Cartigories = SubCatNewRate;
-  await rates.save();
-  res.status(200).json({
-    status: "success",
-    message: "Rate successfully set",
-  });
+  if (Cat_SubBodyObj.type === "catigory") {
+    const catNewRate = [...rates.gitCard_Cartigories, Cat_SubBodyObj];
+    rates.gitCard_Cartigories = catNewRate;
+    await rates.save();
+    res.status(200).json({
+      status: "success",
+      message: `Rate successfully set ${Cat_SubBodyObj.type} `,
+    });
+  }
+  if (Cat_SubBodyObj.type === "SubCatigory") {
+    const SubCatNewRate = [...rates.giftCardSub_Cartigories, Cat_SubBodyObj];
+    rates.giftCardSub_Cartigories = SubCatNewRate;
+    await rates.save();
+    res.status(200).json({
+      status: "success",
+      message: `Rate successfully set ${Cat_SubBodyObj.type} Rate`,
+    });
+  }
 });
 
 exports.setgiftcardSub_Catigory = catchAsync(async (req, res, next) => {
