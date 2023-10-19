c68a029d32f9896cdd458f1ea4d1aaaa40cd6c9f
ABDULLAHI <Ashleyalvins010@gmail.com>
2023-10-19 11:26:57 +0100

    Updates Transactions.js

--- controller/Admin/Transactions.js
+++ controller/Admin/Transactions.js
@@ -58,15 +58,7 @@ exports.userTransation = catchAsync(async (req, res, next) => {
   for (const entry of userTransation) {
     const rateAlart = entry.userId[0].rateAlart;
     const userData = entry.userId[0];
-    const {
-      name,
-      email,
-      phoneNumber,
-      accounName,
-      accountNumber,
-      bankName,
-      walletBalance,
-    } = userData;
+    const { accounName, accountNumber, bankName, walletBalance } = userData;
 
     const bankdetails = {
       accounName,
@@ -74,23 +66,7 @@ exports.userTransation = catchAsync(async (req, res, next) => {
       bankName,
       walletBalance,
     };
-    // const transactions = {
-    //   userId: [
-    //     {
-    //       name,
-    //       email,
-    //       phoneNumber,
-    //     },
-    //   ],
-    //   amount: entry.amount,
-    //   currency: entry.currency,
-    //   createdAt: entry.createdAt,
-    //   status: entry.status,
-    //   access_code: entry.access_code,
-    //   txId: entry.txId,
-    //   id: entry.id,
-    // };
-
+    console.log(bankdetails);
     res.status(200).json({
       status: "success",
       rateAlart,
