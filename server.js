de65a5e9351dbbe4ca2c8988ffb321e4e4509d6d
ABDULLAHI <Ashleyalvins010@gmail.com>
2023-10-19 18:48:01 +0100

    Updates server.js

--- server.js
+++ server.js
@@ -93,7 +93,9 @@ app.use("/api/V1/skyshowNG", Admin);
 app.use("/notify", notifications);
 
 app.all("*", (req, res, next) => {
-  return next(new AppError(`cant find this ${req.originalUrl} on this server`));
+  return next(
+    new AppError(`cant find this ${req.originalUrl} on this server`, 200)
+  );
 });
 
 app.use(globalHandler);
