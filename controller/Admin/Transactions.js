f602e82a428d636c0c9ceb9dbbcf05c16c23e83c
ABDULLAHI <Ashleyalvins010@gmail.com>
2023-10-19 11:53:02 +0100

    Updates Transactions.js

--- controller/Admin/Transactions.js
+++ controller/Admin/Transactions.js
@@ -25,36 +25,6 @@ exports.userTransation = catchAsync(async (req, res, next) => {
       "name email phoneNumber walletBalance accounName accountNumber bankName rateAlart",
   });
 
-  // const rateAlart = userTransation.map((item) => {
-  //   return {
-  //     asset: item.userId[0].rateAlart[0].asset,
-  //     enteredAmount: item.userId[0].rateAlart[0].enteredAmount,
-  //     selectedCategory: item.userId[0].rateAlart[0].selectedCategory,
-  //     selectedNotifyMethod: item.userId[0].rateAlart[0].selectedNotifyMethod,
-  //     selectedRate: item.userId[0].rateAlart[0].selectedRate,
-  //     selectedSubCategory: item.userId[0].rateAlart[0].selectedSubCategory,
-  //     createdAt: item.userId[0].rateAlart[0].createdAt,
-  //     _id: item.userId[0].rateAlart[0]._id,
-  //     id: item.userId[0].rateAlart[0]._id,
-  //   };
-  // });
-
-  // const bankdetails = userTransation.map((item) => {
-  //   return {
-  //     accounName: item.userId[0].accounName,
-  //     accountNumber: item.userId[0].accountNumber,
-  //     bankName: item.userId[0].bankName,
-  //     walletBalance: item.userId[0].walletBalance,
-  //   };
-  // });
-
-  // Create the 'allTransactions' array
-  // const allTransactions = userTransation.map((item) => {
-  //   return {
-  //     amount: item[0].amount,
-  //   };
-  // });
-
   for (const entry of userTransation) {
     const rateAlart = entry.userId[0].rateAlart;
     const userData = entry.userId[0];
@@ -66,7 +36,6 @@ exports.userTransation = catchAsync(async (req, res, next) => {
       bankName,
       walletBalance,
     };
-    console.log(bankdetails);
     res.status(200).json({
       status: "success",
       rateAlart,
