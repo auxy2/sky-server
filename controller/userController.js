c5005ddec731d1a9af9d35067dc58e03961fef46
ABDULLAHI <Ashleyalvins010@gmail.com>
2023-10-20 17:31:28 +0100

    Updated

--- controller/userController.js
+++ controller/userController.js
@@ -216,7 +216,7 @@ exports.viewCryptoRates = catchAsync(async (req, res, next) => {
     if (rate.cryptoRate.product === "eth") rate === ethRates;
     if (rate.cryptoRate.product === "Usdt") rate === usdtRates;
   }
-  console.log(rates.cryptoRate.produt);
+  console.log(rates.cryptoRate.product);
   res.status(200).json({
     status: "success",
     btcRates,
