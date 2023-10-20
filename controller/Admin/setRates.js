fe4c281cbbea8c4fbb8a87cae032913bfa376ba7
ABDULLAHI <Ashleyalvins010@gmail.com>
2023-10-20 18:40:35 +0100

    Updated

--- controller/Admin/setRates.js
+++ controller/Admin/setRates.js
@@ -4,69 +4,14 @@ const AppError = require("../../routes/utills/AppError");
 const catchAsync = require("../../routes/utills/catchAsync");
 
 exports.setRate = catchAsync(async (req, res, next) => {
-  // const user = await User.findOne(req.user).select("role");
-  // const admin = await User.findOne({ role: "admin" });
-  // console.log(user);
-  // const { btcRate, ethRate, usdtRate } = req.query;
-
-  // const rates = await Rates.findOne({ Admin: "Admin" });
-
-  // // if (user.role === "admin" || user.role === "maneger") {
-  // if (!rates.btcRate) {
-  //   await Rates.create({
-  //     btcRate: btcRate,
-  //   });
-  //   res.status(200).json({
-  //     status: "success",
-  //     message: "Btc Rate Set Successfully",
-  //   });
-  // }
-  // if (ethRate) {
-  //   await Rates.create({
-  //     ethRate: ethRate,
-  //   });
-  //   res.status(200).json({
-  //     status: "success",
-  //     message: "Rate Set Successfully",
-  //   });
-  // }
-
-  // if (usdtRate) {
-  //   await Rates.create({
-  //     usdtRate: usdtRate,
-  //   });
-  //   res.status(200).json({
-  //     status: "success",
-  //     message: "Rate Set Successfully",
-  //   });
-  // }
-  // } else {
-  //   res.status(404).json({
-  //     status: "failed",
-  //     message: "you dont have access to use this resource",
-  //   });
-  // }
-
-  // if(!admin || !req.query.password){
-  //     return next(new AppError('you are not permited to use this page'))
-  // }
-  // if(req.query.password){
-  //     if(!admin.correctPass(password, admin.password)){
-  //         return next(new AppError('Incorrect Password', 403))
-  //     }
-
-  //                 }
-  //             }
-  //         })
-  //     })
-  // }
-
   const rate = await Rates.findOne({ Admin: "Admin" });
 
+  if (rate.cryptoRate.length === 3) {
+    return next(new AppError("maxim rate is set", 200));
+  }
   const newRate = [...rate.cryptoRate, req.body];
   rate.cryptoRate = newRate;
   await rate.save();
-  console.log(req.body);
   res.status(200).json({
     status: "success",
     message: `you seccessfully set ${req.body.product} rate`,
