f42dcb0e87f8f3016527fef7cb7b1b725af1a035
ABDULLAHI <Ashleyalvins010@gmail.com>
2023-10-21 21:23:39 +0100

    Updated

--- controller/Admin/users.js
+++ controller/Admin/users.js
@@ -69,6 +69,9 @@ exports.getUser = catchAsync(async (req, res, next) => {
 
 exports.addUsers = catchAsync(async (req, res, next) => {
   const user = await User.create(req.body);
+
+  user.verify = "verified";
+  await user.save({ validateBeforeSave: false });
   res.status(201).json({
     status: "sucess",
     message: "you successfully created a new user",
