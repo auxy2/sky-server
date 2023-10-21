153499513b9f4ab6c716c1b80992268fecaca849
ABDULLAHI <Ashleyalvins010@gmail.com>
2023-10-21 22:20:16 +0100

    Updated

--- controller/transactions/NGN/ngn.js
+++ controller/transactions/NGN/ngn.js
@@ -8,10 +8,10 @@ const trns = require("../../../models/TransactoinsModel");
 
 exports.withdraw = catchAsync(async (req, res, next) => {
   const user = await User.findOne({ email: req.user.email });
-  const text = String(req.query.desc || "");
+  const text = String(req.body.desc || "");
   const ref = crypto.randomBytes(8).toString("hex");
   const balance = parseFloat(String(user.walletBalance).replace(/,/g, ""));
-  const amount = `${req.query.amount}00`;
+  const amount = `${req.body.amount}00`;
   //   console.log("new", amount);
 
   if (!user) {
@@ -28,7 +28,7 @@ exports.withdraw = catchAsync(async (req, res, next) => {
 
   const transferData = {
     source: "balance",
-    reason: req.query.desc,
+    reason: req.body.desc,
     amount: amount,
     recipient: "",
     reference: "",
@@ -37,70 +37,75 @@ exports.withdraw = catchAsync(async (req, res, next) => {
   // const amount = transferData.amount.toLocaleString().toFixed(2)
   // console.log('amount', amount)
 
-  if (!text.toLowerCase().includes("crypto") && !text.startsWith("cryp")) {
-    if (parseFloat(balance) < parseFloat(amount)) {
-      console.log(parseFloat(balance), parseFloat(amount));
-      const resp = await axios.post(transferRecipient, recipientData, {
-        headers: {
-          Authorization: `Bearer ${process.env.PAYSTACK_KEY}`,
-          "Content-Type": "Application/json",
-        },
-      });
-
-      const recipientCode = resp.data.data.recipient_code;
-      const refr = ref.length > 16 ? ref.slice(0, 16) : ref;
-      transferData.reference = refr;
-      transferData.recipient = recipientCode;
-
-      const response = await axios.post(transfer, transferData, {
-        headers: {
-          Authorization: `Bearer ${process.env.PAYSTACK_KEY}`,
-          "Content-Type": "Application/json",
-        },
-      });
-      console.log(response.data.message);
-      if (response.data.message === "Transfer has been queued") {
-        let newBalance;
-        console.log("queue");
-
-        const data = response.data.data;
-        // console.log(newAmount);
-        const trxObj = {
-          amount: data.amount,
-          txId: data.reference,
-          currency: data.currency,
-          access_code: data.transfer_code,
-          userId: user.id,
-        };
-        const balance = parseFloat(
-          String(user.walletBalance).replace(/,/g, "")
-        );
-        const amount = (trxObj.amount / 100).toFixed(2);
-        console.log("amount", amount);
-
-        newBalance = parseFloat(balance - amount).toFixed(2);
-
-        console.log(newBalance);
-
-        user.walletBalance = Number(newBalance).toLocaleString();
-        await user.save({ validateBeforeSave: false });
-
-        const newTx = await trns.create(trxObj);
-        console.log(user.id, newTx);
-        res.status(200).json({
-          status: "success",
-          wallet_Balance: newBalance.toLocaleString(),
+  if (!user.transactionPin) {
+    return next(new AppError("You have not set transaction pin yet", 200));
+  } else if (user.transactionPin === req.body.pin) {
+    if (!text.toLowerCase().includes("crypto") && !text.startsWith("cryp")) {
+      if (parseFloat(balance) < parseFloat(amount)) {
+        console.log(parseFloat(balance), parseFloat(amount));
+        const resp = await axios.post(transferRecipient, recipientData, {
+          headers: {
+            Authorization: `Bearer ${process.env.PAYSTACK_KEY}`,
+            "Content-Type": "Application/json",
+          },
         });
+
+        const recipientCode = resp.data.data.recipient_code;
+        const refr = ref.length > 16 ? ref.slice(0, 16) : ref;
+        transferData.reference = refr;
+        transferData.recipient = recipientCode;
+
+        const response = await axios.post(transfer, transferData, {
+          headers: {
+            Authorization: `Bearer ${process.env.PAYSTACK_KEY}`,
+            "Content-Type": "Application/json",
+          },
+        });
+        if (response.data.message === "Transfer has been queued") {
+          let newBalance;
+          console.log("queue");
+
+          const data = response.data.data;
+          // console.log(newAmount);
+          const trxObj = {
+            amount: data.amount,
+            txId: data.reference,
+            currency: data.currency,
+            access_code: data.transfer_code,
+            userId: user.id,
+          };
+          const balance = parseFloat(
+            String(user.walletBalance).replace(/,/g, "")
+          );
+          const amount = (trxObj.amount / 100).toFixed(2);
+          console.log("amount", amount);
+
+          newBalance = parseFloat(balance - amount).toFixed(2);
+
+          console.log(newBalance);
+
+          user.walletBalance = Number(newBalance).toLocaleString();
+          await user.save({ validateBeforeSave: false });
+
+          const newTx = await trns.create(trxObj);
+          console.log(user.id, newTx);
+          res.status(200).json({
+            status: "success",
+            wallet_Balance: Number(newBalance).toLocaleString().toFixed(2),
+          });
+        } else {
+          return next(new AppError("something went wrong", 400));
+        }
       } else {
-        return next(new AppError("something went wrong", 400));
+        console.log(balance, req.body.amount);
+        return next(new AppError("Your Balance Is Too Low", 200));
       }
     } else {
-      console.log(balance, req.query.amount);
-      res.send("Your Balance Is Too Low");
+      return next(
+        new AppError("crypto is not a valid option for description", 400)
+      );
     }
   } else {
-    return next(
-      new AppError("crypto is not a valid option for description", 400)
-    );
+    return next(new AppError("incorrect pin", 200));
   }
 });
