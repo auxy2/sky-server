0a135c0772b58da03725940c0564b481b617f413
ABDULLAHI <Ashleyalvins010@gmail.com>
2023-10-21 08:11:52 +0100

    Updated

--- controller/Admin/rateAlarts.js
+++ controller/Admin/rateAlarts.js
@@ -2,9 +2,34 @@ const user = require("../../models/userModel");
 const catchAsync = require("../../routes/utills/catchAsync");
 
 exports.getRateAlarts = catchAsync(async (req, res, next) => {
-  const alarts = await user.findOne({});
+  const alarts = await user.find({});
+
+  const groupedData = alarts.map((item) => {
+    const groupedItem = {
+      name: item.name,
+      phoneNumber: item.phoneNumber,
+      email: item.email,
+      username: item.username,
+      rateAlarts: [],
+    };
+
+    if (item.rateAlart) {
+      groupedItem.rateAlarts = item.rateAlart.map((rateAlert) => ({
+        asset: rateAlert.asset,
+        enteredAmount: rateAlert.enteredAmount,
+        selectedCategory: rateAlert.selectedCategory,
+        selectedNotifyMethod: rateAlert.selectedNotifyMethod,
+        selectedRate: rateAlert.selectedRate,
+        selectedSubCategory: rateAlert.selectedSubCategory,
+        createdAt: rateAlert.createdAt,
+      }));
+    }
+
+    return groupedItem;
+  });
+
   res.status(200).json({
     status: "success",
-    alarts,
+    groupedData,
   });
 });
