dbf3b76c00fecf99b754dacecd18834fe78b25db
ABDULLAHI <Ashleyalvins010@gmail.com>
2023-10-25 22:47:13 +0100

    Updated

--- controller/Admin/tutorials.js
+++ controller/Admin/tutorials.js
@@ -14,53 +14,40 @@ exports.Uploads = catchAsync(async (req, res, next) => {
     const ImageBufer = req.files.image[0].buffer;
     console.log("image", ImageBufer, "video", videoBuffer);
 
-    cloudinary.uploader
-      .upload_stream(
-        req.file.path,
-        { resource_type: "image" },
-        async (err, ImageResult) => {
-          if (err) {
-            console.log(err.message);
-            return next(new AppError("Error Uploading video", 200));
-          }
-
-          cloudinary.uploader
-            .upload_stream(
-              req.file.path,
-              { resource_type: "video" },
-              async (err, VideoResult) => {
-                if (err) {
-                  console.log(err.message);
-                  return next(new AppError("Error Uploading video", 200));
-                }
-
-                const rate = await Rates.findOne({ Admin: "Admin" });
-
-                const body = req.body;
-                body.coverImage = ImageResult.url;
-                body.video = VideoResult.url;
-                body.console.log(
-                  "body request",
-                  req.body,
-                  body,
-                  "image",
-                  ImageResult,
-                  "video",
-                  VideoResult
-                );
-
-                const newtutorials = [...rate.tutorials, body];
-                rate.tutorials = newtutorials;
-                res.status(200).json({
-                  status: "success",
-                  message: "Video Uploads succesfull",
-                });
-              }
-            )
-            .end(videoBuffer);
+    cloudinary.uploader.upload(
+      req.file.path,
+      { resource_type: "video" },
+      async (err, ImageResult) => {
+        if (err) {
+          console.log(err.message);
+          return next(new AppError("Error Uploading video", 200));
         }
-      )
-      .end(ImageBufer);
+
+        // cloudinary.uploader
+        //   .upload_stream(
+        //     req.file.path,
+        //     { resource_type: "video" },
+        //     async (err, VideoResult) => {
+        //       if (err) {
+        //         console.log(err.message);
+        //         return next(new AppError("Error Uploading video", 200));
+        // }
+
+        const rate = await Rates.findOne({ Admin: "Admin" });
+
+        const body = req.body;
+        body.coverImage = ImageResult.url;
+        body.video = ImageResult.url;
+        body.console.log("body request", req.body, body, "image", ImageResult);
+
+        const newtutorials = [...rate.tutorials, body];
+        rate.tutorials = newtutorials;
+        res.status(200).json({
+          status: "success",
+          message: "Video Uploads succesfull",
+        });
+      }
+    );
   } else {
     return next(new AppError("no video found", 200));
   }
