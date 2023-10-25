7afd6d5af96d05265178428da3a37e6169502fb0
ABDULLAHI <Ashleyalvins010@gmail.com>
2023-10-25 15:47:49 +0100

    Updated

--- controller/Admin/setRates.js
+++ controller/Admin/setRates.js
@@ -50,7 +50,6 @@ exports.setRate = catchAsync(async (req, res, next) => {
 
 exports.setGiftCardRate = catchAsync(async (req, res, next) => {
   const rates = await Rates.findOne({ Admin: "Admin" });
-  console.log(req.body);
 
   let Cat_SubBodyObj = req.body;
 
@@ -60,7 +59,6 @@ exports.setGiftCardRate = catchAsync(async (req, res, next) => {
 
   if (Cat_SubBodyObj.type === "catigory") {
     if (req.file) {
-      const catNewRate = [...rates.gitCard_Cartigories, Cat_SubBodyObj];
       cloudinary.uploader.upload(req.file.path, async (err, result) => {
         if (err) {
           return next(new AppError("image uploads fail", 200));
