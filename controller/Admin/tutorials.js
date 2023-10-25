b3c15ea04df39b533d784f7b315e464870d0b604
ABDULLAHI <Ashleyalvins010@gmail.com>
2023-10-25 17:13:15 +0100

    Updated

--- controller/Admin/tutorials.js
+++ controller/Admin/tutorials.js
@@ -5,22 +5,27 @@ const cloudinary = require("cloudinary").v2;
 
 exports.Uploads = catchAsync(async (req, res, next) => {
   if (req.file) {
-    cloudinary.uploader.upload(req.file.path, (err, result) => {
-      if (err) {
-        return next(new AppError("Error Uploading video", 200));
+    cloudinary.uploader.upload(
+      req.file.path,
+      { resource_type: "video" },
+      (err, result) => {
+        if (err) {
+          console.log(err.message);
+          return next(new AppError("Error Uploading video", 200));
+        }
+
+        const rate = Rates.findOne({ Admin: "Admin" });
+
+        const body = req.body;
+        body.url = result.url;
+        const newtutorials = [...rate.tutorials, req.body];
+        rate.tutorials = newtutorials;
+        res.send(200).json({
+          status: "success",
+          message: "Video Uploads succesfull",
+        });
       }
-
-      const rate = Rates.findOne({ Admin: "Admin" });
-
-      const body = req.body;
-      body.url = result.url;
-      const newtutorials = [...rate.tutorials, req.body];
-      rate.tutorials = newtutorials;
-      res.send(200).json({
-        status: "success",
-        message: "Video Uploads succesfull",
-      });
-    });
+    );
   } else {
     return next(new AppError("no video found", 200));
   }
