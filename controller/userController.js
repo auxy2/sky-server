ff07942c2fae4e749c17c9a10f0d7c77bf621b7a
ABDULLAHI <Ashleyalvins010@gmail.com>
2023-10-29 00:57:43 +0100

    Updated

--- controller/userController.js
+++ controller/userController.js
@@ -158,19 +158,15 @@ exports.updateMe = catchAsync(async (req, res, next) => {
   }
   if (req.file) {
     console.log(req.file.path);
-    cloudinary.uploader.upload(
-      req.file.path,
-      { resource_type: "image" },
-      async (err, ImageResult) => {
-        if (err) {
-          console.log(err.message);
-          return next(new AppError("Error Uploading video", 200));
-        }
-        UpdatedUser.profilePhoto = ImageResult.url;
-        console.log(UpdatedUser, ImageResult.url);
-        await UpdatedUser.save({ validateBeforeSave: false });
+    cloudinary.uploader.upload(req.file.path, async (err, ImageResult) => {
+      if (err) {
+        console.log(err.message);
+        return next(new AppError("Error Uploading video", 200));
       }
-    );
+      UpdatedUser.profilePhoto = ImageResult.url;
+      console.log(UpdatedUser, ImageResult.url);
+      await UpdatedUser.save({ validateBeforeSave: false });
+    });
     console.log(req.file);
   }
   const reUpdateUser = {
