9afea2d18bd47c23e94dc5c82881559019e2975f
ABDULLAHI <Ashleyalvins010@gmail.com>
2023-11-12 19:00:22 +0100

    Updated

--- controller/Admin/Transactions.js
+++ controller/Admin/Transactions.js
@@ -5,11 +5,11 @@ const catchAsync = require("../../routes/utills/catchAsync");
 exports.viewAllTrns = catchAsync(async (req, res, next) => {
   const trnx = await trns.find().populate({
     path: "userId",
-    select: "name email phoneNumber profilePhoto -__v",
+    select: "name email phoneNumber profilePhoto",
   });
   const giftCard_trnx = await giftCard.find().populate({
     path: "userId",
-    select: "name email phoneNumber profilePhoto -__v",
+    select: "name email phoneNumber profilePhoto",
   });
 
   res.status(200).json({
