1a3d04888dab80a8aaab6e1e16d0bf6e33e5f0f2
Abdullahi <105906731+auxy2@users.noreply.github.com>
2024-04-03 22:39:13 +0100

    Update sms.js

--- routes/utills/sms.js
+++ routes/utills/sms.js
@@ -1,22 +1,22 @@
-const twilio = require('twilio');
+// const twilio = require('twilio');
 
-const accountSid = process.env.TWILIO_ACCOUNT_SID
-const authToken = process.env.YOUR_TWILIO_AUTH_TOKEN;
-const twilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER;
+// const accountSid = process.env.TWILIO_ACCOUNT_SID
+// const authToken = process.env.YOUR_TWILIO_AUTH_TOKEN;
+// const twilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER;
 
-const client = twilio(accountSid, authToken);
+// const client = twilio(accountSid, authToken);
 
-// Function to send the SMS
-exports.sendSMS = message => {
-  client.messages
-    .create({
-      body: message,
-      from: twilioPhoneNumber,
-      to: '+234'+message.phoneNumber
-    })
-    .then(message => console.log('SMS sent successfully:', message.sid))
-    .catch(error => console.error('Error sending SMS:', error));
-}
+// // Function to send the SMS
+// exports.sendSMS = message => {
+//   client.messages
+//     .create({
+//       body: message,
+//       from: twilioPhoneNumber,
+//       to: '+234'+message.phoneNumber
+//     })
+//     .then(message => console.log('SMS sent successfully:', message.sid))
+//     .catch(error => console.error('Error sending SMS:', error));
+// }
 
 
 
